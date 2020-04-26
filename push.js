3e9435bae6980eaccce5da1963217e7d4e05605e
rajesh.kumar <rajesh.kumar@smaketsolutions.com>
2020-04-26 21:35:03 +0530

    modyfy push file

--- push.js
+++ push.js
@@ -11,7 +11,12 @@ push.setVapidDetails(
     Vapidkeys.publicKey,
     Vapidkeys.privateKey
 );
-const sub = {endpoint: "https://fcm.googleapis.com/fcm/send/fuPW7A1sZaQ:AP…6SpWmViOFDo2HP1XHgWUL4JPxP-_V9W93GbVr9BbzqI0gZVCu",
- expirationTime: null,
-  options: PushSubscriptionOptions};
+const sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fuPW7A1sZaQ:APA91bFFLSq4BI6BcAb2hX8dao0kTp_M7TQkP6Rh562MEEPlYnevdUxBeuqLbuF6LczVK65IIPoTfhCp-VZbY3kqJYN6SpWmViOFDo2HP1XHgWUL4JPxP-_V9W93GbVr9BbzqI0gZVCu",
+"expirationTime":null,
+"keys":
+{
+    "p256dh":"BCf8iipAAbzIVgWxeAf9TnU7b7O_o1B-pSjcx_zZuVBruHbBpO8lUyenuPezbLAH5W7Wf89ooMmbj7eywYu7m-I",
+    "auth":"2V86CJHchg8LSIhd-WcMkA"
+}
+            };
 push.sendNotification(sub, 'test message');
