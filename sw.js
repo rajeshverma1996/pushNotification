1b1a5cf80fe49f47df495c65059d9248db7f8543
rajesh.kumar <rajesh.kumar@smaketsolutions.com>
2020-04-27 00:44:48 +0530

    service worker file change

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', function(e) {
     var options = {
-      body: 'This notification was generated from a push!',
+      body: 'hello indrajeet how are u today!',
       icon: 'images/example.png',
       vibrate: [100, 50, 100],
       data: {
